6ab7f1551ae2a89bc5e65a3ed083a3597b194dc8
Joe Lewis <jlewis23@umd.edu>
2022-01-05 18:47:03 -0500

    Added service supervisor to instance container

--- instance/run_instance.sh
+++ instance/run_instance.sh
@@ -4,16 +4,4 @@
 
 # Since notebook is behind a cool proxy (by me), we need to change the baseURL with the hostname
 sed -i "s|%contID%|$HOSTNAME|g" /home/jupyter/.jupyter/jupyter_notebook_config.py
-# Start supervisord
-#/usr/bin/supervisord
-
-# Start Flask
-/usr/bin/python3 /usr/src/app/app.py &
-P1=$!
-
-# Start Jupyter
-su jupyter -s /bin/sh -c "cd /home/jupyter/; jupyter notebook --no-browser" &
-P2=$!
-
-wait $P1 $P2
-
+exec runsvdir /usr/src/app/services
