eb31ff77e11a0dcc52932dcd616e871bfbb0fc0d
Joe Lewis <jlewis23@umd.edu>
2022-03-10 02:18:06 -0500

    Added load-basic to dev-ob.sh and uploaded basic tasks
    load-basic command now loads basic task database into task generator
    manager command now creates a python venv if one dosen't already exist

--- dev-ob.sh
+++ dev-ob.sh
@@ -14,19 +14,20 @@ GREEN='\033[32m'
 NC='\033[0m' # No Color
 
 usage() {
-    echo "Developer Observatory Control Script"
-    echo "Usage: dev-ob.sh [command]"
-    echo -e "\nAvailable commands:"
-    echo -e "install\t\t Installs all required packages and sets up services"
-    echo -e "generate\t Runs the notebook task generator"
-    echo -e "configure\t Generates configuration files"
-    echo -e "run\t\t Configures and runs the docker application in current terminal"
-    echo -e "manager\t\t Runs the manager script. Should be run at the same time as the primary app"
-    echo -e "recreate\t Given a service name, recreates a running container with new configuration"
-    echo -e "down\t\t Alias for compose down"
-    echo -e "reset\t\t Completely resets the application to the inital state - ALL DATA IS DELETED"
-    echo -e "compose\t\t Any commands after compose will be sent to a docker-compose with the correct arguments"
-    echo -e "docs\t\t Start a HTTP server to view documentation in doc/ directory"
+  echo "Developer Observatory Control Script"
+  echo "Usage: dev-ob.sh [command]"
+  echo -e "\nAvailable commands:"
+  echo -e "install\t\t Installs all required packages and sets up services"
+  echo -e "generate\t Runs the notebook task generator"
+  echo -e "configure\t Generates configuration files"
+  echo -e "run\t\t Configures and runs the docker application in current terminal"
+  echo -e "manager\t\t Runs the manager script. Should be run at the same time as the primary app"
+  echo -e "recreate\t Given a service name, recreates a running container with new configuration"
+  echo -e "down\t\t Alias for compose down"
+  echo -e "reset\t\t Completely resets the application to the inital state - ALL DATA IS DELETED"
+  echo -e "compose\t\t Any commands after compose will be sent to a docker-compose with the correct arguments"
+  echo -e "load-basic\t Loads basic programming tasks into generator. \n\t\t Helpful for running the developer observatory without creating tasks"
+  echo -e "docs\t\t Start a HTTP server to view documentation in doc/ directory"
 }
 
 prompt_confirm() {
@@ -41,99 +42,99 @@ prompt_confirm() {
 }
 
 build_config() {
-    if [[ -d "generator/generated" ]]; then
-        echo "Generating configuration files and secrets"
-        cp -r generator/generated containers/submit/tasks/
-    else
-        echo -ne "${RED} It seems there are no tasks generated. Please generate "
-        echo -e  "tasks first before configuring. ${NC}"
-        exit 1
-    fi
+  if [[ -d "generator/generated" ]]; then
+    echo "Generating configuration files and secrets"
+    cp -r generator/generated containers/submit/tasks/
+  else
+    echo -ne "${RED}It seems there are no tasks generated. Please generate "
+    echo -e  "tasks first before configuring. ${NC}"
+    exit 1
+  fi
 
-    # Generate a logLevel variable based on the appMode. Used for python
-    # logging modules.
-    if [[ $appMode == "DEBUG" ]]; then
-      logLevel="DEBUG"
-    else
-      logLevel="INFO"
-    fi
+  # Generate a logLevel variable based on the appMode. Used for python
+  # logging modules.
+  if [[ $appMode == "DEBUG" ]]; then
+    logLevel="DEBUG"
+  else
+    logLevel="INFO"
+  fi
 
-    # Configuring secrets, since this has persistence
-    if [[ ! -f config/.secrets ]]; then
-        #Generate Passwords for the database in the first run. Replace in the files directly
-        pwUser0=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-        pwUser1=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-        pwUser2=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-        pwUser3=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-        submitSecret=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  # Configuring secrets, since this has persistence
+  if [[ ! -f config/.secrets ]]; then
+    #Generate Passwords for the database in the first run. Replace in the files directly
+    pwUser0=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    pwUser1=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    pwUser2=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    pwUser3=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    submitSecret=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
-        echo -e "#!/bin/bash\npwUser0=$pwUser0\npwUser1=$pwUser1\npwUser2=$pwUser2\npwUser3=$pwUser3\nsubmitSecret=$submitSecret" > config/.secrets
-    else
-        source config/.secrets
-    fi
+    echo -e "#!/bin/bash\npwUser0=$pwUser0\npwUser1=$pwUser1\npwUser2=$pwUser2\npwUser3=$pwUser3\nsubmitSecret=$submitSecret" > config/.secrets
+  else
+    source config/.secrets
+  fi
 
-    # Copy config files into place
-
-
-    # Landing
-    cp config/landing.php containers/landing/webpageConf/config.php
-    sed -i "s|%pwUser1%|$pwUser1|g" containers/landing/webpageConf/config.php
-    sed -i "s|%finalSurveyURL%|$finalSurveyURL|g" containers/landing/webpageConf/config.php
-    sed -i "s|%dailyMaxInstances%|$dailyMaxInstances|g" containers/landing/webpageConf/config.php
-    sed -i "s|%maxInstances%|$maxInstances|g" containers/landing/webpageConf/config.php
-    sed -i "s|%recaptchaSiteKey%|$recaptchaSiteKey|g" containers/landing/webpageConf/config.php
-    sed -i "s|%recaptchaSecret%|$recaptchaSecret|g" containers/landing/webpageConf/config.php
-    sed -i "s|%awsLang%|$awsLang|g" containers/landing/webpageConf/config.php
-    sed -i "s|%awsAccessKey%|$awsAccessKey|g" containers/landing/webpageConf/config.php
-    sed -i "s|%awsSecretKey%|$awsSecretKey|g" containers/landing/webpageConf/config.php
-    sed -i "s|%awsRegion%|$awsRegion|g" containers/landing/webpageConf/config.php
-    sed -i "s|%awsImageId%|$awsImageId|g" containers/landing/webpageConf/config.php
-    sed -i "s|%awsInstanceType%|$awsInstanceType|g" containers/landing/webpageConf/config.php
-    sed -i "s|%awsSecurityGroupID%|$awsSecurityGroupID|g" containers/landing/webpageConf/config.php
-    sed -i "s|%sshKeyName%|$awsSshKeyName|g" containers/landing/webpageConf/config.php
-    sed -i "s|%poolSize%|$poolSize|g" containers/landing/webpageConf/config.php
-    sed -i "s|%tokenGetUrl%|$tokenGetUrl|g" containers/landing/webpageConf/config.php
-    sed -i "s|%tokenSetUrl%|$tokenSetUrl|g" containers/landing/webpageConf/config.php
-
-    # Control
-    cp config/control_config.py containers/control/config.py
-    sed -i "s|%pwUser2%|$pwUser2|g" containers/control/config.py
-    sed -i "s|%logLevel%|$logLevel|g" containers/control/config.py
-    sed -i "s|%instanceIdleTime%|$instanceIdleTime|g" containers/control/config.py
+  # Copy config files into place
 
 
-    # Submit
-    cp config/submit.py containers/submit/configSubmit.py
-    sed -i "s|%pwUser2%|$pwUser2|g" containers/submit/configSubmit.py
-    sed -i "s|%finalSurveyURL%|$finalSurveyURL|g" containers/submit/configSubmit.py
-    sed -i "s|%submitSecret%|$submitSecret|g" containers/submit/configSubmit.py
+  # Landing
+  cp config/landing.php containers/landing/webpageConf/config.php
+  sed -i "s|%pwUser1%|$pwUser1|g" containers/landing/webpageConf/config.php
+  sed -i "s|%finalSurveyURL%|$finalSurveyURL|g" containers/landing/webpageConf/config.php
+  sed -i "s|%dailyMaxInstances%|$dailyMaxInstances|g" containers/landing/webpageConf/config.php
+  sed -i "s|%maxInstances%|$maxInstances|g" containers/landing/webpageConf/config.php
+  sed -i "s|%recaptchaSiteKey%|$recaptchaSiteKey|g" containers/landing/webpageConf/config.php
+  sed -i "s|%recaptchaSecret%|$recaptchaSecret|g" containers/landing/webpageConf/config.php
+  sed -i "s|%awsLang%|$awsLang|g" containers/landing/webpageConf/config.php
+  sed -i "s|%awsAccessKey%|$awsAccessKey|g" containers/landing/webpageConf/config.php
+  sed -i "s|%awsSecretKey%|$awsSecretKey|g" containers/landing/webpageConf/config.php
+  sed -i "s|%awsRegion%|$awsRegion|g" containers/landing/webpageConf/config.php
+  sed -i "s|%awsImageId%|$awsImageId|g" containers/landing/webpageConf/config.php
+  sed -i "s|%awsInstanceType%|$awsInstanceType|g" containers/landing/webpageConf/config.php
+  sed -i "s|%awsSecurityGroupID%|$awsSecurityGroupID|g" containers/landing/webpageConf/config.php
+  sed -i "s|%sshKeyName%|$awsSshKeyName|g" containers/landing/webpageConf/config.php
+  sed -i "s|%poolSize%|$poolSize|g" containers/landing/webpageConf/config.php
+  sed -i "s|%tokenGetUrl%|$tokenGetUrl|g" containers/landing/webpageConf/config.php
+  sed -i "s|%tokenSetUrl%|$tokenSetUrl|g" containers/landing/webpageConf/config.php
 
-    # Manager
-    cp config/manager_config.py manager/
-    sed -i "s|%instancesNetwork%|$instancesNetwork|g" manager/manager_config.py
-    sed -i "s|%poolSize%|$poolSize|g" manager/manager_config.py
-    sed -i "s|%logLevel%|$logLevel|g" manager/manager_config.py
-    sed -i "s|%dockerProjectName%|$dockerProjectName|g" manager/manager_config.py
+  # Control
+  cp config/control_config.py containers/control/config.py
+  sed -i "s|%pwUser2%|$pwUser2|g" containers/control/config.py
+  sed -i "s|%logLevel%|$logLevel|g" containers/control/config.py
+  sed -i "s|%instanceIdleTime%|$instanceIdleTime|g" containers/control/config.py
 
-    # Postgres
-    mkdir -p containers/postgres
-    cp config/Postgres.docker containers/postgres/Dockerfile
-    cp config/dbSchema.sql containers/postgres/
-    cp generator/generated/dbSchema.sql containers/postgres/taskSchema.sql
-    sed -i "s|%pwUser0%|$pwUser0|g" containers/postgres/Dockerfile
-    sed -i "s|%pwUser1%|$pwUser1|g" containers/postgres/dbSchema.sql
-    sed -i "s|%pwUser2%|$pwUser2|g" containers/postgres/dbSchema.sql
-    sed -i "s|%pwUser3%|$pwUser3|g" containers/postgres/dbSchema.sql
 
-    # Instance
-    cp instance/template/app.py instance/app.py
-    cp instance/template/custom.js instance/jupyter/
-    sed -i "s|%landingURL%|$landingURL|g" instance/app.py
-    sed -i "s|%appMode%|$appMode|g" instance/app.py
-    sed -i "s|%submitSecret%|$submitSecret|g" instance/app.py
-    sed -i "s|%landingURL%|$landingURL|g" instance/jupyter/custom.js
-    sed -i "s|%skippedTaskSurveyURL%|$skippedTaskSurveyURL|g" instance/jupyter/custom.js
-    sed -i "s|%taskCount%|$taskCount|g" instance/jupyter/custom.js
+  # Submit
+  cp config/submit.py containers/submit/configSubmit.py
+  sed -i "s|%pwUser2%|$pwUser2|g" containers/submit/configSubmit.py
+  sed -i "s|%finalSurveyURL%|$finalSurveyURL|g" containers/submit/configSubmit.py
+  sed -i "s|%submitSecret%|$submitSecret|g" containers/submit/configSubmit.py
+
+  # Manager
+  cp config/manager_config.py manager/
+  sed -i "s|%instancesNetwork%|$instancesNetwork|g" manager/manager_config.py
+  sed -i "s|%poolSize%|$poolSize|g" manager/manager_config.py
+  sed -i "s|%logLevel%|$logLevel|g" manager/manager_config.py
+  sed -i "s|%dockerProjectName%|$dockerProjectName|g" manager/manager_config.py
+
+  # Postgres
+  mkdir -p containers/postgres
+  cp config/Postgres.docker containers/postgres/Dockerfile
+  cp config/dbSchema.sql containers/postgres/
+  cp generator/generated/dbSchema.sql containers/postgres/taskSchema.sql
+  sed -i "s|%pwUser0%|$pwUser0|g" containers/postgres/Dockerfile
+  sed -i "s|%pwUser1%|$pwUser1|g" containers/postgres/dbSchema.sql
+  sed -i "s|%pwUser2%|$pwUser2|g" containers/postgres/dbSchema.sql
+  sed -i "s|%pwUser3%|$pwUser3|g" containers/postgres/dbSchema.sql
+
+  # Instance
+  cp instance/template/app.py instance/app.py
+  cp instance/template/custom.js instance/jupyter/
+  sed -i "s|%landingURL%|$landingURL|g" instance/app.py
+  sed -i "s|%appMode%|$appMode|g" instance/app.py
+  sed -i "s|%submitSecret%|$submitSecret|g" instance/app.py
+  sed -i "s|%landingURL%|$landingURL|g" instance/jupyter/custom.js
+  sed -i "s|%skippedTaskSurveyURL%|$skippedTaskSurveyURL|g" instance/jupyter/custom.js
+  sed -i "s|%taskCount%|$taskCount|g" instance/jupyter/custom.js
 }
 
 checkCompose() {
@@ -166,10 +167,6 @@ elif [[ $1 == "generate" ]]; then
     -t "$dockerProjectName-generator" \
     --build-arg uid=$UID
 
-  if [[ ! -d generator/generated ]]; then
-    mkdir generator/generated
-  fi
-
   echo "Finished building task generator"
 
   mkdir -p $PWD/generator/generated
@@ -191,8 +188,16 @@ elif [[ $1 == "run" ]]; then
 
   runCompose build && runCompose up
 elif [[ $1 == "manager" ]]; then
+  if [[ ! -d manager/pyenv ]]; then
+    echo "Creating venv for manager..."
+    python3 -m venv manager/pyenv
+    source ./manager/pyenv/bin/activate
+    pip install -r manager/requirements.txt
+  else
+    source ./manager/pyenv/bin/activate
+  fi
+
   build_config
-  source ./manager/pyenv/bin/activate
   python3 ./manager/app.py
 
 elif [[ $1 == "recreate" ]]; then
@@ -234,6 +239,18 @@ elif [[ $1 == "reset" ]]; then
 elif [[ $1 == "compose" ]]; then
   shift
   runCompose $@
+
+elif [[ $1 == "load-basic" ]]; then
+  if [[ -f generator/tmp/db.sqlite ]]; then
+    echo -e "${RED}ERROR: Refusing to overwrite existing task database.${NC}"
+    echo -e "Please backup or remove files in generator/tmp/ before continuing"
+    exit 1
+  fi
+
+  mkdir -p generator/tmp
+  cp config/basic_tasks.sqlite generator/tmp/db.sqlite
+  echo "Basic tasks loaded into generator. Please run dev-ob.sh generate and \
+generate the task files from the web interface"
 
 elif [[ $1 == "docs" ]]; then
   python3 -m http.server --directory doc/html
