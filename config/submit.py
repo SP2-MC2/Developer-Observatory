05fb72d4c6db54abcad5d4d4cb939efa1482b6a2
Joe Lewis <jlewis23@umd.edu>
2022-03-11 15:18:20 -0500

    Submit container now runs just flask
    Removed uwsgi-nginx-flask container in favor of a plain python container
    Flask app no longer runs as root
    get_ipynb endpoint now IP restricted

--- config/submit.py
+++ config/submit.py
@@ -1,4 +1,4 @@
 SQLALCHEMY_DATABASE_URI = "postgresql://insert_user:%pwUser2%@db/notebook"
 FINAL_SURVEY_URL =  "%finalSurveyURL%"
-TASKFILES_BASE_PATH = "/app/tasks/"
+TASKFILES_BASE_PATH = "/home/user/app/tasks/"
 SUBMIT_SECRET = "%submitSecret%"
--- containers/submit/submit.py
+++ containers/submit/submit.py
@@ -204,4 +204,4 @@ def submit():
 
 if __name__ == '__main__':
     #app.debug = True
-    app.run(host='localhost', port=6200)
+    app.run(host='0.0.0.0', port=8000)
